f1328c7dd07a27c5c0247e9ccb2054b09140cce3
Mickael Gerard <mikarun@gmail.com>
2021-05-11 20:41:17 +0200

    update package naming

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   sanity: {
-    projectId:
-      process.env.GATSBY_SANITY_PROJECT_ID || "ltptw7jt",
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "ltptw7jt",
     dataset: process.env.GATSBY_SANITY_DATASET || "production",
   },
 };
